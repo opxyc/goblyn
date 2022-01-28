5336386aa4f7d313e6ac34afdb00add965893b80
Bony Emmanuel <95271796+be-v@users.noreply.github.com>
2022-01-28 12:20:46 +0530

    feat: accept query params

--- main.go
+++ main.go
@@ -15,15 +15,20 @@ import (
 
 type JSONFileContent struct {
 	Paths []struct{
-		Path string 		`json:"path"`
-		Get *Response 		`json:"get"`
-		Post *Response 	`json:"post"`
+		Path string 				`json:"path"`
+		Get *ReqStructure 	`json:"get"`
+		Post *ReqStructure 	`json:"post"`
 	} `json:"paths"`
 }
 
-type Response struct {
+type Params []string
+
+type ReqStructure struct {
+	Params Params `json:"params"`
 	Response json.RawMessage `json:"response"`
 }
+
+var r *mux.Router
 
 func main() {
 	filePath := flag.String("f", "", "path to file with data to mock")
@@ -51,28 +56,44 @@ func main() {
 		log.Fatalf("could not unmarshal json file: %v", err)
 	}
 
-	r := mux.NewRouter()
+	r = mux.NewRouter()
 
 	fmt.Println("identified paths:")
 	for _, path := range fileContent.Paths {
-		p := path.Path
 		if path.Get != nil {
-			fmt.Println("GET ", p)
-			r.HandleFunc(path.Path, func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Add("Content-Type", "application/json")
-				writeResponse(path.Get.Response, w)
-			}).Methods(http.MethodGet)
+			RegisterNewGetRoute(path.Path, path.Get)
 		}
 		if path.Post != nil {
-			fmt.Println("POST", p)
-			r.HandleFunc(path.Path, func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Add("Content-Type", "application/json")
-				writeResponse(path.Post.Response, w)
-			}).Methods(http.MethodPost)
+			RegisterNewPostRoute(path.Path, path.Post)
 		}
 	}
 	
 	http.ListenAndServe(*address, r)
+}
+
+func RegisterNewGetRoute(path string, req *ReqStructure) {
+	fmt.Println("GET ", path, "?", req.Params)
+	query := []string{}
+	for _, param := range req.Params {
+		query = append(query, param, fmt.Sprintf("{%s}", param))
+	}
+	r.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL)
+		w.Header().Add("Content-Type", "application/json")
+		writeResponse(req.Response, w)
+	}).Methods(http.MethodGet).Queries(query...)
+}
+
+func RegisterNewPostRoute(path string, req *ReqStructure) {
+	fmt.Println("POST", path, "?", req.Params)
+	query := []string{}
+	for _, param := range req.Params {
+		query = append(query, param, fmt.Sprintf("{%s}", param))
+	}
+	r.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		writeResponse(req.Response, w)
+	}).Methods(http.MethodPost).Queries(query...)
 }
 
 func writeResponse(res json.RawMessage, w io.Writer) {
